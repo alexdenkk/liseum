3df5e31de1479428e383070d4e6989f93635babf
alexdenkk <alexdenkk@yandex.ru>
2024-02-10 19:14:35 +0700

    -

--- cmd/liseum/main.go
+++ cmd/liseum/main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"alexdenkk/liseum/model"
 	"alexdenkk/liseum/pkg/db"
+	"alexdenkk/liseum/pkg/hash"
 	"alexdenkk/liseum/server"
 	"flag"
 	"log"
 )
 
 var (
-	addr   string
-	jwt    string
-	dbName string
-	dbPort string
-	dbUser string
-	dbPswd string
+	addr          string
+	jwt           string
+	dbName        string
+	adminLogin    string
+	adminPassword string
 )
 
 func main() {
+
 	parseFlags()
 
 	// connecting to db
@@ -33,6 +34,22 @@ func main() {
 	model.Migrate(serverDB)
 	log.Println("migration complete")
 
+	// creating admin from server
+	if adminLogin != "" && adminPassword != "" {
+		result := serverDB.Create(&model.User{
+			Login:    adminLogin,
+			Password: hash.Hash(adminPassword),
+		})
+
+		if result.Error != nil {
+			log.Fatal(err)
+			return
+		}
+
+		log.Println("user created")
+		return
+	}
+
 	// create app
 	booksApp := server.New(serverDB, []byte(jwt), addr)
 	log.Println("server initialized")
@@ -45,5 +62,7 @@ func parseFlags() {
 	flag.StringVar(&addr, "address", ":8080", "address and(or) port for app")
 	flag.StringVar(&jwt, "jwt", "fuck", "jwt sign key for user tokens")
 	flag.StringVar(&dbName, "dbname", "liseum.db", "database name")
+	flag.StringVar(&adminLogin, "admin-login", "", "admin login")
+	flag.StringVar(&adminPassword, "admin-pass", "", "admin password")
 	flag.Parse()
 }
